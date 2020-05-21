1eabb14c25bc37acd5e4bfbe767bde56f9a0b767
Perry Lucas <plucas@sei.com>
2020-05-21 16:22:46 -0400

    tsc

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,10 +2,6 @@ export interface SqlResult {
     sql?: string;
     params?: string[];
 }
-export declare class SqlResultImpl {
-    sql?: string;
-    params?: string[];
-}
 export interface QueryOptions {
     dbType?: string;
     filtering?: string;
@@ -14,14 +10,12 @@ export interface QueryOptions {
     page?: string;
     size?: string;
 }
-export declare class QueryOptionsImpl implements QueryOptions {
-}
 export default class LoadSql {
     sqlDir: string;
     sqlCache: any;
     constructor(sqlDir: string);
-    load(file: string, q?: QueryOptions): Promise<SqlResult | undefined>;
+    load(file: string, q?: QueryOptions): Promise<SqlResult>;
     loadForMysql(file: string, q?: QueryOptions): Promise<SqlResult>;
-    loadForMssql(file: string, q?: QueryOptions): SqlResult | undefined;
+    loadForMssql(file: string, q?: QueryOptions): Promise<SqlResult>;
 }
 //# sourceMappingURL=../index.d.ts.map
